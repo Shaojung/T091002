62b3cae6dcb692f2d82c11100bc91979cd94510c
Shaojung <shaojung@gmail.com>
2015-09-10 13:32:47 +0800

    Hibernate setting okay.

--- src/edu/pccu/Student.java
+++ src/edu/pccu/Student.java
@@ -1,5 +1,35 @@
 package edu.pccu;
 
 public class Student {
-
+	private int student_id = 0;
+	private String student_name = null;
+	private String student_tel = null;
+	
+	public int getStudent_id()
+	{
+		return student_id;
+	}
+	
+	public void setStudent_id(int student_id)
+	{
+		this.student_id = student_id;
+	}
+	
+	public String getStudent_name()
+	{
+		return student_name;
+	}
+	public void setStudent_name(String student_name)
+	{
+		this.student_name = student_name;
+	}
+	public String getStudent_tel()
+	{
+		return student_tel;
+	}
+	
+	public void setStudent_tel(String student_tel)
+	{
+		this.student_tel = student_tel;
+	}
 }
